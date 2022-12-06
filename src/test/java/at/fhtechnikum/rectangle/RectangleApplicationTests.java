6ce0eb60e83117fb93feb1c330e26653505f37e7
Alexander Brait <ic20b057@technikum-wien.at>
2022-12-06 20:45:40 +0100

    #2 added TestCases

--- src/test/java/at/fhtechnikum/rectangle/RectangleApplicationTests.java
+++ src/test/java/at/fhtechnikum/rectangle/RectangleApplicationTests.java
@@ -1,13 +1,28 @@
 package at.fhtechnikum.rectangle;
 
+import at.fhtechnikum.rectangle.controller.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class RectangleApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void convInchTest() {
+		controller cont = new controller();
+		String result = cont.convert_inch(1);
+		assertEquals(result,0.9144);
+
+	}
+
+	@Test
+	void convMeterTest() {
+		controller cont = new controller();
+		String result = cont.convert_meter(1);
+		assertEquals(result,2.54);
+
 	}
 
 }
