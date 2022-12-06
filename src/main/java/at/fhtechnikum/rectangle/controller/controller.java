ff5d22c3f08f45adf478ea5840d2b2e5cf1370fc
Alexander Brait <ic20b057@technikum-wien.at>
2022-12-06 19:59:46 +0100

    #1 added Methods for conversion to Centimeter/Inch

--- src/main/java/at/fhtechnikum/rectangle/controller/controller.java
+++ src/main/java/at/fhtechnikum/rectangle/controller/controller.java
@@ -1,5 +1,18 @@
 package at.fhtechnikum.rectangle.controller;
 
-public class controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class controller {
+    @GetMapping("/api/centimeter?inch={inch}")
+    public double convert_inch(@PathVariable double inch){
+        return inch * 2.54;
+    }
+
+    @GetMapping("/api/meter?yard={centimeter}")
+    public double convert_centimeter(@PathVariable double centimeter){
+        return centimeter * 0.9144;
+    }
 }
